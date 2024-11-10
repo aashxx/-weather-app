c66288148a86618fac95ee703b1bb40d50870006
Mohamed Aashir <tmohamedaashir@gmail.com>
2024-11-10 18:42:08 +0530

    updated after 1 year

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=';
+const weatherUrl = 'http://api.weatherapi.com/v1/current.json?key=e7854d004d164449b0e125022241011&aqi=no&q=';
+const unsplashAccessKey = 'BsJWqrIHiShH-g_lintqWQ3jHsPOXd1wwcWdxgsZhmQ'; 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a6bbc42ef5mshdd1f4812dc62175p1b0a64jsnc39b20198a83',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
 };
 
 const getWeather = (city) => {
     cityName.innerHTML = city;
-    fetch(url + city,options).then(response => response.json()).then(response => {
-        console.log(response)
-        temp.innerHTML = response.temp 
-        humidity.innerHTML = response.humidity
-        min_temp.innerHTML = response.min_temp
-        max_temp.innerHTML = response.max_temp
-        wind_speed.innerHTML = response.wind_speed
-    }).catch(err => console.error(err));
-    document.body.style.background = "url('https://source.unsplash.com/random/1600x900/?" +city+ "/')";
-}
+
+    // Fetch weather data
+    fetch(weatherUrl + city, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            temp.innerHTML = response.current.temp_c;
+            humidity.innerHTML = response.current.humidity;
+            min_temp.innerHTML = response.current.feelslike_c;
+            max_temp.innerHTML = response.current.dewpoint_c;
+            wind_speed.innerHTML = response.current.wind_kph;
+        })
+        .catch(err => console.error('Error fetching weather data:', err));
+
+    const unsplashUrl = `https://api.unsplash.com/search/photos?query=${city}&client_id=${unsplashAccessKey}&orientation=landscape&per_page=1`;
+
+    fetch(unsplashUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data.results.length > 0) {
+                const imageUrl = data.results[0].urls.regular;
+                document.body.style.backgroundImage = `url('${imageUrl}')`;
+                document.body.style.backgroundSize = "cover";
+                document.body.style.backgroundPosition = "center";
+            } else {
+                console.error("No image found for the specified city.");
+            }
+        })
+        .catch(error => console.error('Error fetching image from Unsplash:', error));
+};
 
 getWeather("Chennai");
 
 submit_search.addEventListener("click", (e) => {
     getWeather(city.value);
-})
+});
 
 city.addEventListener("keyup", (e) => {
-    if(e.key == "Enter") {
+    if (e.key === "Enter") {
         getWeather(city.value);
     }
-})
-
+});
